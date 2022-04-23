faec4c31c3c798dd7c18af838d6852205478c7aa
I Putu Nanda Amanta <nansamgr@gmail.com>
2022-04-23 18:43:30 +0800

    Time: 7 ms (61.56%), Space: 44.9 MB (68.76%) - LeetHub

---
+++ 13-roman-to-integer/13-roman-to-integer.java
@@ -0,0 +1,54 @@
+class Solution {
+   public int romanToInt(String s) {
+        char[] array = s.toCharArray();
+
+        int result = 0;
+        for (int i = array.length - 1; i >= 0; i--) {
+            if (result == 0) {
+                result += this.convert(array[i]);
+                continue;
+            }
+
+            if (this.convert(array[i]) == this.convert(array[i + 1])) {
+                result += this.convert(array[i]);
+                continue;
+            }
+
+
+            if (result > this.convert(array[i])) {
+                result -= this.convert(array[i]);
+                continue;
+            }
+
+            if (result <= this.convert(array[i])) {
+                result += this.convert(array[i]);
+                continue;
+            }
+        }
+       
+        return result;    
+   }
+
+   
+
+    private int convert(char c) {
+        switch (c) {
+            case 'I':
+                return 1;
+            case 'V':
+                return 5;
+            case 'X':
+                return 10;
+            case 'L':
+                return 50;
+            case 'C':
+                return 100;
+            case 'D':
+                return 500;
+            case 'M':
+                return 1000;
+
+        }
+        return 0;
+    }
+}
