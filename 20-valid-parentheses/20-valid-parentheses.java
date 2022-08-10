f3fe2b698c8f7cf794d99921de4b2d74784970c5
I Putu Nanda Amanta <nansamgr@gmail.com>
2022-08-10 15:24:13 +0800

    Time: 4 ms (31.76%), Space: 42.1 MB (42.60%) - LeetHub

--- 20-valid-parentheses/20-valid-parentheses.java
+++ 20-valid-parentheses/20-valid-parentheses.java
@@ -5,7 +5,6 @@ class Solution {
         if (currentElement == ')' || currentElement == '}' || currentElement == ']' || s.length() % 2 == 1) {
             return false;
         }
-        
         for (int i = 1; i < s.length(); i++) {
             char element = s.charAt(i);
             if (sb.length() > 0) {
