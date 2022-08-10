d5b8d7e22ffa8aa4406e4795bb9a449aa3d1d560
I Putu Nanda Amanta <nansamgr@gmail.com>
2022-08-10 15:18:06 +0800

    Time: 3 ms (62.77%), Space: 42.4 MB (18.09%) - LeetHub

---
+++ 20-valid-parentheses/20-valid-parentheses.java
@@ -0,0 +1,25 @@
+class Solution {
+    public boolean isValid(String s) {
+       char currentElement = s.charAt(0);
+        var sb = new StringBuilder().append(currentElement);
+        if (currentElement == ')' || currentElement == '}' || currentElement == ']' || s.length() % 2 == 1) {
+            return false;
+        }
+
+        for (int i = 1; i < s.length(); i++) {
+            char element = s.charAt(i);
+            if (sb.length() > 0) {
+                if ((sb.charAt(sb.length() - 1) == '(' && element == ')')
+                        || (sb.charAt(sb.length() - 1) == '[' && element == ']')
+                        || (sb.charAt(sb.length() - 1) == '{' && element == '}')) {
+                    sb.deleteCharAt(sb.length() - 1);
+                    continue;
+                }
+
+            }
+
+            sb.append(element);
+        }
+        return sb.length() == 0;
+    }
+}
