8122193dd42004f7d61749cfac55c97fcf78bd9c
I Putu Nanda Amanta <nansamgr@gmail.com>
2022-10-30 14:20:39 +0800

    Time: 1 ms (20.02%), Space: 43.8 MB (17.35%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.java
+++ 0035-search-insert-position/0035-search-insert-position.java
@@ -2,17 +2,14 @@ class Solution {
     public int searchInsert(int[] nums, int target) {
         int indexLocation = 0;
         for(int i = 0; i< nums.length ; i++){
-            
             if(i == nums.length - 1 && nums[i] < target){
                 indexLocation = nums.length;
                 break;
-            }
-            
+            } 
             if(nums[i] == target){
                 indexLocation = i;
                 break;
             }
-            
             if(nums[i] > target){
                 indexLocation = i;
                 break;
